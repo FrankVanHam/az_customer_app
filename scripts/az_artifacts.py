c540877e111ac0f1c89d6ef57e31d84aee96698a
FrankVanHam <frnkvnhm@gmail.com>
2026-02-11 20:24:08 +0100

    bit debug

--- scripts/az_artifacts.py
+++ scripts/az_artifacts.py
@@ -7,6 +7,7 @@ class azArtifacts:
     def download(self, feed, artifact_name, path, project, source_file):
         if not os.path.isfile(source_file):
             print(f"Downloading {artifact_name} to {source_file}")
+            print(f"parameters: {[feed, artifact_name, path, project]}")
             azRunner().run(['artifacts', 'universal', 'download', '--feed', feed, '--name', artifact_name, '--path', path, '--version', '*', '--project', project, '--scope', 'project']) 
         else:
             print(f"reusing existing download {source_file}")
