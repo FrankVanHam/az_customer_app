521bb9e2d33daff8a507cf3352a6fd76a0970165
FrankVanHam <frnkvnhm@gmail.com>
2026-02-12 17:49:11 +0100

    kkk

--- scripts/az_runner.py
+++ scripts/az_runner.py
@@ -13,4 +13,5 @@ class azRunner:
                 os.chmod(sh_file, st.st_mode | stat.S_IEXEC)
             args = ["sh", sh_file]
         args.extend(p_args)
+        print(args)
         subprocess.run(args)
