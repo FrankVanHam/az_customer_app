46d3cb9b4e7b143c788575ed93539cce491e5314
FrankVanHam <frnkvnhm@gmail.com>
2026-02-13 12:10:00 +0100

    try again

--- scripts/az_zipper.py
+++ scripts/az_zipper.py
@@ -6,4 +6,8 @@ class azZipper:
         pass
     def unzip(self, the_file, target):
         print(f"Unzipping {the_file} to {target}")
-        azRunner().run_bare(['unzip', the_file, target])
+        if os.name == 'nt':
+            print(['7z', 'x', '-o'+target, the_file])
+            azRunner().run_bare(['7z', 'x', '-y', '-o'+target, the_file])
+        else:
+            azRunner().run_bare(['unzip', the_file,'-d', target])
